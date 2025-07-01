80382b60d0638092b5f777266becc0347f4e6e8f
Elixir-Exoprosopa <raviravirego@gmail.com>
2025-07-01 19:05:22 -0300

    Update EnigmaTiersMod.java

--- src/EnigmaInside/EnigmaTiersMod.java
+++ src/EnigmaInside/EnigmaTiersMod.java
@@ -1,8 +1,8 @@
 package EnigmaInside;
 import arc.util.Log;
-import EnigmaTiers.content.*;
+import EnigmaInside.content.*;
 import mindustry.game.Schematics;
-import mindustry.mod.Mod*;
+import mindustry.mod.Mod;
 
 public class EnigmaTiersMod extends Mod {
     public ETMain(){
