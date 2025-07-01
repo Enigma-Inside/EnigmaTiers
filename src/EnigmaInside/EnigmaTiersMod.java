596d14817e6d0f2ce8205ade22e1976c782f3ce3
Elixir-Exoprosopa <raviravirego@gmail.com>
2025-07-01 19:01:33 -0300

    Update EnigmaTiersMod.java

--- src/EnigmaInside/EnigmaTiersMod.java
+++ src/EnigmaInside/EnigmaTiersMod.java
@@ -6,10 +6,6 @@ import mindustry.mod.*;
 
 public class EnigmaTiersMod extends Mod {
 @Override
-public ETMain(){
-        Log.info("Loaded Enigma Tiers constructor.");
-    }
-@Override
 public void loadContent(){
 ETBlocks.load();   
 }
